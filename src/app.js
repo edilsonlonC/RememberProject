976cdd6d85cff77e7210af2e408cd4269b4e6639
Edilson <eddylson.londonio@gmail.com>
2021-08-01 18:12:27 -0500

    ignore folders

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
 import { config } from './config/config';
+const port = config.port
+
+const app = express();
+
+/**  Middleware **/
+app.use(morgan('dev'))
 app.listen(port, () => {});
 export default app;
--- src/config/config.js
+++ src/config/config.js
@@ -4,5 +4,6 @@ export const config = {
     process.env.secretKeyRefreshToken || 'secretKeyRefreshToken',
   defaultLang: process.env.defaultLang || 'es',
   versionApi: process.env.versionApi || '/api/v1',
-  minExpiredSession: process.env.minExpiredSession || 3600000 * 3,
+	minExpiredSession: process.env.minExpiredSession || 3600000 * 3,
+	port: process.env.PORT || 9000,
 };
